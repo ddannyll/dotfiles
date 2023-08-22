3295dc06ef5bf72eadbe1a4cef48781f0b54e398
Daniel Nguyen <daniel.nguyen@scaccelerator.io>
2023-08-22 13:08:37 +1000

    update custom

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -1,6 +1,21 @@
 local overrides = require 'custom.overrides'
 local plugins = {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
    'abecodes/tabout.nvim',
     lazy=false,
     config = function()
