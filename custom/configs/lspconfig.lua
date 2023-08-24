ddf251d54f5b26b9609da32698f269d3ee887c2b
Daniel Nguyen <daniel.nguyen@scaccelerator.io>
2023-08-24 14:47:50 +1000

    fix gopls lsp

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -4,23 +4,23 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
-lspconfig.gopls.setup {
-  on_attack = on_attach,
-  capabilities = capabilities,
-  cmd = {"gopls"},
-  filetypes = {"go", "gomod", "gowork", "gotmpl"},
-  root_dir = util.root_pattern("go.work", "go.mpd", ".git"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
-}
+-- lspconfig.gopls.setup {
+--   on_attack = on_attach,
+--   capabilities = capabilities,
+--   cmd = {"gopls"},
+--   filetypes = {"go", "gomod", "gowork", "gotmpl"},
+--   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+--   settings = {
+--     gopls = {
+--       completeUnimported = true,
+--       analyses = {
+--         unusedparams = true,
+--       },
+--     },
+--   },
+-- }
 
-local servers = {"tsserver", "eslint"}
+local servers = {"tsserver", "eslint", "gopls"}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
