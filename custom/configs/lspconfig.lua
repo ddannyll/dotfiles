b6bcd985eed1da0a2509d89994fe5abf94bf2c11
Daniel Nguyen <daniel.nguyen@scaccelerator.io>
2023-08-29 16:29:36 +1000

    eslint fmt on save

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -27,3 +27,12 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.eslint.setup {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+}
