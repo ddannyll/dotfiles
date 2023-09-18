fa5ecb5fc905a815ecada7041d6d263780fb3b2b
ddannyll <daniel.nguyen.s173@gmail.com>
2023-09-19 09:57:35 +1000

    fix matchit

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -13,4 +13,42 @@ M.ui = {
 
 M.plugins = 'custom.plugins'
 M.mappings = require "custom.mappings"
+M.lazy_nvim = {
+  performance = {
+    rtp = {
+      -- Comment plugins that you want to leave enabled:
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        -- "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+        "editorconfig",
+      }
+    }
+  }
+}
 return M
+
