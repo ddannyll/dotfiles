7efa5362b1e84fd38f511773c420b207d62859d0
ddannyll <daniel.nguyen.s173@gmail.com>
2023-09-19 10:17:48 +1000

    add nulls

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -2,21 +2,21 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local util = require "lspconfig/util"
+local util = require("lspconfig/util")
 
-local servers = {"tsserver", "eslint", "gopls", "prismals", "cssls", "tailwindcss"}
+local servers = { "tsserver", "eslint", "gopls", "prismals", "cssls", "tailwindcss" }
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-lspconfig.eslint.setup {
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-  end,
-}
+lspconfig.eslint.setup({
+	on_attach = function(client, bufnr)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			command = "EslintFixAll",
+		})
+	end,
+})
--- custom/plugins.lua
+++ custom/plugins.lua
@@ -53,12 +53,19 @@ local plugins = {
       ensure_installed = {
         "gopls",
         "typescript-language-server",
-        "eslint-lsp"
+        "eslint-lsp",
+        "stylua",
       },
     },
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+       "jose-elias-alvarez/null-ls.nvim",
+     config = function()
+       require "custom.configs.null-ls"
+     end,
+    },
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
