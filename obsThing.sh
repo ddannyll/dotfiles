78ab37d5650fe06fe7abca82bcaa20b379ae9c7e
ddannyll <daniel.nguyen.s173@gmail.com>
2024-05-19 00:29:07 +1000

    delete all

--- obsThing.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo modprobe v4l2loopback
--- symlink.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-if [ $# -ne 2 ]
-then
-  echo "Usage: $0 <file> <linkPath>"
-fi
-
-pathDotfile=$(realpath "$1")
-ln -s "$pathDotfile" $2
